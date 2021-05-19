67fb89f998afbc0c4434019e3f6b25ef90a9da9e
Ayush Singh <ayushsingh.as1700@gmail.com>
2021-05-20 01:12:07 +0530

    Update Blockchain.java

--- Blockchain.java
+++ Blockchain.java
@@ -4,10 +4,10 @@ public class Blockchain {
 
 	public static void main(String[] args) {
 		
-		String[] genesisTransactions = {"Elon made Ayush a Billionaire by giving him 1000 bitcoin", "Ayush gave away all the bitcoins to poor people because he is a nobal guy"};
+		String[] genesisTransactions = {"Elon made Ayush a Billionaire by giving him 1000 bitcoin", "Ayush gave away all the bitcoins to poor people because he is a noble guy"};
 		Block genesisBlock = new Block(0, genesisTransactions);      //Creating Genesis block with some random transactions
 		
-		String[] block2Transactions = {"Bill gave away 10000 bitcoin to the people in Africa", "Gates foundation gets 10000 bitcoin of donation from some unknown source, Africa suspected"};
+		String[] block2Transactions = {"Bill gave away 10000 bitcoins to the people in Africa", "Gates foundation recieved 10000 bitcoins of donation from some unknown source, Africa suspected"};
 		Block block2 = new Block(genesisBlock.getBlockHash(), block2Transactions);  //Creating Second block with some random transactions
 		
 		genesisBlock.next = block2;       // Creating chains or joining Block where this.block contains previous block address(Hashs)
